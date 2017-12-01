4ad51ad967a0614d34e21234c06f55549eda12cc
surajbhukebag <surajbhukebag@gmail.com>
2017-11-30 18:56:01 -0800

    Admin related changes

--- client/modules/Admin/AdminActions.js
+++ client/modules/Admin/AdminActions.js
@@ -1,41 +1,49 @@
 import * as API from './api/AdminApi';
+import callApi from './../../util/apiCaller';
 
 // Export Constants
 export const CREATE_FLIGHT = 'CREATE_FLIGHT';
-
+export const ADMIN_SIGNIN = 'ADMIN_SIGNIN';
+export const ADMIN_SIGNOUT = 'ADMIN_SIGNOUT';
 
 // Export Actions
 export function createFlight(data) {
-	
-	let req = {};
-	req.flightID = data.flightNumber;
-	let price = {economy: data.economyPrice, business: data.businessPrice, firstClass: data.firstPrice }
-	req.price = price;
-	req.operator = data.operator;
-	req.departureTime = data.depTime;
-	req.arrivalTime = data.arvTime;
-	req.origin = data.from;
-	req.destination = data.to;
-	req.duration = 1;
 
-	return function(dispatch) {
-		return  API.createFlight(req)
-			    	.then((resData) => {
-			    		if(resData.code === 502) {
-			    			dispatch(createFlightResponse()); 
-			    		}
-			    		else {
-			    			dispatch(createFlightResponse()); 	
-			    		}
-				        
-	      		});
-	  	};
+  return (dispatch) => {
+    return callApi('/flight', 'post', data).then(res => dispatch(createFlightResponse(res)));
+  };
 
 }
 
 function createFlightResponse() {
-	return {
-		type: CREATE_FLIGHT,
-		msg : "New Flight Created"
-	}
+  return {
+    type: CREATE_FLIGHT,
+    msg: "New Flight Created"
+  }
+}
+
+export function adminsignin(data) {
+  let req = {};
+  req.email = data.email;
+  req.password = data.password;
+  req.isAdmin = true;
+
+  return (dispatch) => {
+    return callApi('/user/login', 'post', req).then(res => dispatch(adminlogin(res)));
+  };
+}
+
+function adminlogin(responseData) {
+	console.log("ss ", responseData)
+  return {
+    type: ADMIN_SIGNIN,
+    data: responseData,
+    status: responseData.status
+  }
+}
+
+export function adminsignout() {
+   return {
+    type: ADMIN_SIGNOUT
+  }  
 }
