fbad676580c33eb6dce9e19ea2c9c4ed33984116
Chintan Vachhani <chintan.vachhani10@gmail.com>
2017-12-01 23:03:50 -0800

    Fixed the repo to keep the earlier changes for backend.

--- kafka-backend/services/hotel.services.js
+++ kafka-backend/services/hotel.services.js
@@ -1,25 +1,164 @@
 let Hotel = require('../models/hotel');
+let cuid = require('cuid');
 
 function handle_request(req, callback) {
   console.log("In handle request:" + JSON.stringify(req));
 
   let res;
 
-  if (req.name === 'getSomething') {
-    if(error){
+  if (req.name === 'createHotel') {
+    let hotel = Hotel({
+      cuid: cuid(),
+      hotelName: req.body.hotelName,
+      price: req.body.price,
+      star: req.body.star,
+      address: req.body.address,
+      city: req.body.city,
+      state: req.body.state,
+      zipCode: req.body.zipCode,
+      //hotelImage: req.body.hotelImage,
+    });
+    hotel.save(function (error) {
       res = {
-        status: 500,
-        title: 'Error title.',
-        error: {message: 'Error message.'},
+        status: 400,
+        title: 'Invalid data.',
+        error: {message: 'Failed to create hotel.'},
       };
-    } else {
-        res = {
-          status: 200,
-          message: 'Success message.',
-        };
-    }
+      callback(null, res);
+    });
+
+    res = {
+      status: 201,
+      message: 'Successfully created hotel.',
+      hotel: hotel,
+    };
+
     callback(null, res);
   }
+
+  if (req.name === 'getHotel') {
+    Hotel.findOne({cuid: req.params.cuid}, (error, hotel) => {
+      if (error) {
+        res = {
+          status: 404,
+          title: 'Hotel not found.',
+          error: {message: 'Failed to retrieve hotel.'},
+        };
+        callback(null, res);
+      } else {
+        res = {
+          status: 200,
+          message: 'Successfully retrieved hotel.',
+          hotel: hotel,
+        };
+        callback(null, res);
+      }
+    });
+  }
+
+  if (req.name === 'updateHotel') {
+    Hotel.findOneAndUpdate({cuid: req.params.cuid}, req.body, (error, hotel) => {
+      if (error) {
+        res = {
+          status: 404,
+          title: 'Hotel not found.',
+          error: {message: 'Failed to update hotel.'},
+        };
+        callback(null, res);
+      } else {
+        res = {
+          status: 200,
+          message: 'Successfully updated hotel.',
+          hotel: req.body,
+        };
+        callback(null, res);
+      }
+    });
+  }
+
+  if (req.name === 'deleteHotel') {
+    Hotel.findOneAndRemove({cuid: req.params.cuid}, (error, hotel) => {
+      if (error) {
+        res = {
+          status: 404,
+          title: 'Hotel not found.',
+          error: {message: 'Failed to delete hotel.'},
+        };
+        callback(null, res);
+      } else {
+        res = {
+          status: 200,
+          message: 'Successfully deleted hotel.',
+        };
+        callback(null, res);
+      }
+    });
+  }
+
+  if (req.name === 'getAllHotels') {
+    Hotel.find({}, (error, hotels) => {
+      if (error) {
+        res = {
+          status: 404,
+          title: 'Hotels not retrieved.',
+          error: {message: 'Failed to retrieve hotels.'},
+        };
+        callback(null, res);
+      } else {
+        res = {
+          status: 200,
+          message: 'Successfully retrieved all hotels.',
+          hotels: hotels,
+        };
+        callback(null, res);
+      }
+    });
+  }
+
+  if (req.name === 'searchHotels') {
+    //Naive logic - to be optimized later
+    let conditions = {};
+    if (req.query.city !== null && req.query.star !== null && req.query.minPrice !== null && req.query.maxPrice !== null) {
+      conditions = {
+        city: req.query.city,
+        star: req.query.star,
+        price: {$gte: req.query.minPrice, $lte: req.query.maxPrice},
+      };
+    } else if (req.query.city !== null && req.query.star !== null) {
+      conditions = {
+        city: req.query.city,
+        star: req.query.star,
+      };
+    } else if (req.query.city !== null && req.query.minPrice !== null && req.query.maxPrice !== null) {
+      conditions = {
+        city: req.query.city,
+        price: {$gte: req.query.minPrice, $lte: req.query.maxPrice},
+      };
+    } else if (req.query.city !== null) {
+      conditions = {
+        city: req.query.city,
+      };
+    }
+
+    Hotel.find(conditions, (error, hotels) => {
+      if (error) {
+        res = {
+          status: 404,
+          title: 'Hotels not retrieved.',
+          error: {message: 'Failed to retrieve hotels.'},
+        };
+        callback(null, res);
+      } else {
+        res = {
+          status: 200,
+          message: 'Successfully retrieved all hotels.',
+          hotels: hotels,
+        };
+        callback(null, res);
+      }
+    });
+  }
+
 }
 
 exports.handle_request = handle_request;
